0981fa8c1b76e68f0e4ecf6ae595a81784e6ed5a
ganxin <ganxin@jiemian.com>
2019-03-03 19:34:58 +0800

    [login]add administrator sign in

---
+++ App/Admin/Controller/IndexController.class.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function login()
+    {
+        // var_dump($_POST);
+        $username = trim(I('post.username'));
+        $password = trim(I('post.pwd'));
+        $re = I('post.re');
+        //echo $re;
+        // echo $password;
+        $result = M('admins')->where(array('name' => $username, 'password' => md5($password)))->find();
+        if (!empty($_COOKIE['password'])) {
+            cookie('password', null);
+        }
+        if (!$result) {
+            $this->error('密码输入错误');
+        } else {
+            session('aid', $result['aid']);
+            if ($re) {
+                cookie('admin', $username, 3600 * 24 * 7);//记住我
+                cookie('password', trim(I('post.pwd')), 3600 * 24 * 7);//记住我
+            } else {
+                cookie('admin', $username, 3600 * 24 * 7);
+            }
+            $url = U('Admin/index');
+            session('pica', $result['pic']);
+            session('aname', $result['name']);
+            header("Location: $url");
+        }
+    }
+
+    public function logout()
+    {
+        session(null);
+        $url = U('Home/Index/index');
+        header("Location: $url");
+    }
+}
