aac15918b88d679a135ed5f573cd8ae27f7c4c53
ganxin <ganxin@jiemian.com>
2019-04-04 23:01:13 +0800

    [update config]use online database

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -4,10 +4,10 @@ return array(
     'URL_MODEL' => 2,
     'DB_CHARSET' => 'utf8',      // 数据库编码默认采用utf8
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'localhost', // 服务器地址
+    'DB_HOST' => '106.14.180.145', // 服务器地址
     'DB_NAME' => 'rachel', // 数据库名
     'DB_USER' => 'root', // 用户名
-    'DB_PWD' => '', // 密码
+    'DB_PWD' => 'dzj06095514', // 密码
     'DB_PORT' => 3306, // 端口
     'DB_PREFIX' => 'rachel_',
     'DATA' => array(
